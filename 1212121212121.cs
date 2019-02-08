77af94d7bc7a2d39a1e0dccbe7196f2b85138f0f
emilpetkovski <47415609+emilpetkovski@users.noreply.github.com>
2019-02-08 09:07:34 +0200

    Add files via upload

---
+++ 1212121212121.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 758;
+            int b = 10;
+            int sum = 758 / 10;
+            Console.WriteLine(758 / 10);
+            Console.WriteLine(758 / 100);
+            Console.WriteLine(758 / 151);
+            Console.WriteLine(758 / 94);
+            Console.WriteLine(758 / 759);
+        }
+    }
+}
